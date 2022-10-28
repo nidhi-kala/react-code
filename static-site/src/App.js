3e79fa4747259a857e7617719a1d5badce94b038
Nidhi Kala <nidhikala95@gmail.com>
2022-10-28 16:26:03 -0400

    seprated: components for header and main"

--- static-site/src/App.js
+++ static-site/src/App.js
@@ -1,22 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
-
+import Main from "./Main";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
-      <main className="facts">
-        <h1>Fun Facts about React</h1>
-        <ul className="list">
-          <li>Was first released in 2013</li>
-          <li>Was orginally created by Jordan Walke</li>
-          <li>Has over 150K stars on GitHub </li>
-          <li>Is Maintianed by Facebook</li>
-          <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
-      </main>
+      <Main />
     </div>
   );
 }
---
+++ static-site/src/Main.js
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function Main() {
+  return (
+    <main className="facts">
+      <h1>Fun Facts about React</h1>
+      <ul className="list">
+        <li>Was first released in 2013</li>
+        <li>Was orginally created by Jordan Walke</li>
+        <li>Has over 150K stars on GitHub </li>
+        <li>Is Maintianed by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </main>
+  );
+}
+
+export default Main;
