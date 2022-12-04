66cc130903c21874db25d2608cca0635e6c21358
Nidhi Kala <nidhikala95@gmail.com>
2022-12-03 22:02:15 -0500

    initial commit

---
+++ static-site/src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Main from "./Main";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <Main />
+    </div>
+  );
+}
+
+export default App;
