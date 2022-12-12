3fb3b87225c20e6ec30f061000e6200125417665
Nidhi Kala <nidhikala95@gmail.com>
2022-12-12 17:50:16 -0500

    update: music added, song updates based on state

--- music-player/src/App.js
+++ music-player/src/App.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+
 import Player from "./components/Player";
 import Song from "./components/Song";
 import "./styles/app.scss";
+import data from "./data";
 function App() {
+  //state
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
   return (
     <div className="App">
       <h1>Music Player</h1>
-      <Song />
-      <Player />
+      <Song currentSong={currentSong} />
+      <Player
+        currentSong={currentSong}
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+      />
     </div>
   );
 }
