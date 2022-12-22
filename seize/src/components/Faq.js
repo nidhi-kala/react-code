23c88fb907ae6a891dc5930ae00432ed43a90300
Nidhi Kala <nidhikala95@gmail.com>
2022-12-21 22:51:04 -0500

    FAQ section added

---
+++ seize/src/components/Faq.js
@@ -0,0 +1,50 @@
+const Faq = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Need to know more? <span>FAQ</span>{" "}
+      </h2>
+      <div className="question">
+        <h1>How do I start?</h1>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque,
+            error
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h1>What will my day look like?</h1>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque,
+            error
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h1>What products do your offer?</h1>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque,
+            error
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h1>What payment methods do you accept?</h1>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque,
+            error
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Faq;
--- seize/src/pages/AboutPage.js
+++ seize/src/pages/AboutPage.js
@@ -1,10 +1,13 @@
 import About from "../components/About";
 import Services from "../components/Services";
+import Faq from "../components/Faq";
+
 const AboutPage = () => {
   return (
     <>
       <About />
       <Services />
+      <Faq />
     </>
   );
 };
